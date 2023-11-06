39fec56e1004443c5f2f9c40978589b83c4e36e5
Vis Virial <visvirial@gmail.com>
2023-11-06 22:33:04 +0900

    Remove `actions/configure-pages`.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	ssr: false,
+	target: 'static',
 	//...
 	build: {
 		transpile: ['vuetify'],
